1bd1b904c94220dfd570be314a46c470055c3f43
slushfrusciante <ffmg12@hotmail.com>
2024-02-06 14:16:10 -0300

    feat: feito chat Bcaju

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# WhatsCaju
-WhatsBcaju
+# App Snapshots
+
+## Snapshot 1 - WhatsApp Page
+![snapshot1](snapshots/snapshot1.png)
+
+## Snapshot 2 - Loading Screen
+![snapshot2](snapshots/snapshot2.png)
---
+++ src/assets/whatsapp.js
@@ -0,0 +1,35 @@
+import pp from "./images/miniheroi.png";
+import cs1 from "./images/cs1.png";
+import cs2 from "./images/cs2.jpeg";
+import chat1 from "./images/miniheroi2.png";
+import chat2 from "./images/chat2.png";
+import chat3 from "./images/chat3.png";
+import chat4 from "./images/chat4.png";
+import chat5 from "./images/chat5.png";
+import chat6 from "./images/chat6.png";
+import chat7 from "./images/chat7.png";
+import chat8 from "./images/boss MS.png";
+import chat9 from "./images/qa-profissional.jpg";
+import chat10 from "./images/dev2.jpg";
+import chat11 from "./images/fit.png";
+import chat12 from "./images/iconbcaju......png";
+import chat13 from "./images/moça.jpg";
+
+export {
+  pp,
+  cs1,
+  cs2,
+  chat1,
+  chat2,
+  chat3,
+  chat4,
+  chat5,
+  chat6,
+  chat7,
+  chat8,
+  chat9,
+  chat10,
+  chat11,
+  chat12,
+  chat13,
+};
